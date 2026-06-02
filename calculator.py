a4c74b3f82d2982f1ed7b256d72d29ca586f7d15
preetu098 <preetu098@gmail.com>
2026-06-02 15:05:55 +0530

    Created OOP calculator

--- calculator.py
+++ calculator.py
@@ -1,23 +1,34 @@
 class Calculator:
     def add(self,a,b):
-        print("Addition ",(a+b))
+        return a+b
     def sub(self,a,b):
-        print("Subtract ",(a-b))
+        return a-b
     def multi(self,a,b):
-        print("Multiply ",(a*b))
+        return a*b
     def divide(self,a,b):
-        print("Divide ",(a/b))
+        if(b==0):
+            return "can't divide by 0"
+        else:
+            return a/b
 
 o=Calculator()
-n1,n2=int(input("Enter first number ")),int(input("Enter second number "))
-sign=input("Enter +,-,*,/ ")
-if(sign=="+"):
-    o.add(n1,n2)
-elif(sign=="-"):
-    o.sub(n1,n2)
-elif(sign=="*"):
-    o.multi(n1,n2)
-elif(sign=="/"):
-    o.divide(n1,n2)
-else:
-    print("Invalid selection")
+v=True
+while(v):
+    try:
+        n1,n2=int(input("Enter first number ")),int(input("Enter second number "))
+    except:
+        print("invalid value ")
+        continue
+    sign=input("Enter +,-,*,/ ,s for stop")
+    if(sign.lower()=="s"):
+        break
+    if(sign=="+"):
+        print("Addition ",o.add(n1,n2))
+    elif(sign=="-"):
+        print("Subtract ",o.sub(n1,n2))
+    elif(sign=="*"):
+        print("Multiply ",o.multi(n1,n2))
+    elif(sign=="/"):
+        print("Divide ",o.divide(n1,n2))
+    else:
+        print("Invalid selection")
